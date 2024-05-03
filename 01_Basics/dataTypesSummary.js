2ce3a238e98c5cd440aea6dae68e057058912595
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-03 07:15:37 +0000

    latest

---
+++ 01_Basics/dataTypesSummary.js
@@ -0,0 +1,23 @@
+//PRIMITIVE DATA TYPES
+//7 types: STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, symbol, BigInt
+
+//JS is dynamically typed language 
+
+//Reference Type DataTypes/non-primituve datatypes
+// Arrays, objects, functions
+
+
+const heroes=["superman","hanuman","shaktiman"];
+
+let myObj= {
+     name: "pankaj",
+    age: 32,
+
+}
+console.log(myObj);
+
+const myfunction = function(){
+    console.log("Hello World Inside function");
+      
+}
+console.log(typeof myfunction);
