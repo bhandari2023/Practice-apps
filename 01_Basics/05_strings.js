99eba71d0ee2b449212e09d9094a398564fb0d13
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-04 06:22:29 +0000

    strings 4 May

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,14 @@
+// const name1="PANKAJBHANDARI";
+
+// const friends= 20;
+
+// console.log(`hello my name is ${name1.toLowerCase()} and I have ${friends} friends`);
+
+const userName= new String('pankaj-bhandari');
+
+console.log(userName[3]);
+console.log(userName.indexOf('b'));
+console.log(userName.split('-'));
+console.log(userName.replaceAll("a","n"));
+console.log(userName.search('-'));
+console.log(userName.slice(-7,-4));
