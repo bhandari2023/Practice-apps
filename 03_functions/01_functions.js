41b087f17add3dedd55574b0314e10cc5d5d3193
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-04 19:01:21 +0000

    functions

---
+++ 03_functions/01_functions.js
@@ -0,0 +1,12 @@
+function sumOfTwoNumber(num1,num2)
+{ 
+    // let result= num1 + num2;
+    // return result;
+    return num1+num2
+
+}
+
+const result=sumOfTwoNumber(1,3);
+
+console.log("result:",result);
+
