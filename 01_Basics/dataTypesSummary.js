456d2da059b692dfa3a5b20630011e435f359c96
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-04 04:48:07 +0000

    4may2024

--- 01_Basics/dataTypesSummary.js
+++ 01_Basics/dataTypesSummary.js
@@ -1,23 +1,45 @@
-//PRIMITIVE DATA TYPES
-//7 types: STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, symbol, BigInt
+// //PRIMITIVE DATA TYPES
+// //7 types: STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, symbol, BigInt
 
-//JS is dynamically typed language 
+// //JS is dynamically typed language 
 
-//Reference Type DataTypes/non-primituve datatypes
-// Arrays, objects, functions
+// //Reference Type DataTypes/non-primituve datatypes
+// // Arrays, objects, functions
 
 
-const heroes=["superman","hanuman","shaktiman"];
+// const heroes=["superman","hanuman","shaktiman"];
 
-let myObj= {
-     name: "pankaj",
-    age: 32,
+// let myObj= {
+//      name: "pankaj",
+//     age: 32,
 
-}
-console.log(myObj);
+// }
+// console.log(myObj);
 
-const myfunction = function(){
-    console.log("Hello World Inside function");
+// const myfunction = function(){
+//     console.log("Hello World Inside function");
       
-}
-console.log(typeof myfunction); 
+// }
+// console.log(typeof myfunction); 
+
+//++++++++++++++++++++++++
+let myProfileName= "pankajbhandari2011@gmail.com";
+
+let myProfileName2=myProfileName
+
+myProfileName2="bhandaripankaj@gmail.com";
+
+console.log(myProfileName);
+console.log(myProfileName2);
+
+let userOne={
+    email: "pankajbhandari2010@gmail.com",
+    pNo: 9950143673,
+};
+
+let userTwo=userOne;
+
+ userTwo.email="user2email@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
