dea868100334f0292931f88e3ec1528c1b329f56
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-04 10:46:19 +0000

    Date and arrays

---
+++ 01_Basics/07_date_time.js
@@ -0,0 +1,15 @@
+// let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString('en-IN'));
+// console.log(myDate.toLocaleDateString('en-IN'));
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-15-2024");
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
