3255e65476fd4690dc392fe281d215b691231c26
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2024-05-04 07:15:52 +0000

    Math and Num

---
+++ 01_Basics/06_nums_math.js
@@ -0,0 +1,42 @@
+// const score= 500;
+
+// console.log(score);
+
+// const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const randomNum = 25;
+// console.log(randomNum.toPrecision(1));
+// console.log(randomNum.toPrecision(2));
+// console.log(randomNum.toPrecision(3));
+// console.log(randomNum.toPrecision(4));
+// console.log(randomNum.toPrecision(5));
+// console.log(randomNum.toPrecision(6));
+// console.log(randomNum.toPrecision(7));
+// console.log(randomNum.toPrecision(8));
+// console.log(randomNum.toPrecision(9));
+const myArray = ["1", "2", "3", "4", "5"];
+// const randomNum2=1000000000000;
+// console.log(randomNum2.toLocaleString('en-US'));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(4));
+// console.log(Math.round(randomNum));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(randomNum));
+// console.log(Math.min(1,2,3,4,5,6,8));
+// console.log(Math.max(1,2,3,4,5,6,8));
+
+console.log(Math.random() * 10 + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
